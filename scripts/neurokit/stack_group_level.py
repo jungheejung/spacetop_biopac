2f117b7c54f302acbd473a4eface5de80eb858eb
jungheejung <>
2022-09-19 10:36:49 -0400

    BLD: combine preprocessed derivatives into one dataframe

---
+++ scripts/neurokit/stack_group_level.py
@@ -0,0 +1,55 @@
+# %%
+# import neurokit2 as nk
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os, glob, sys
+from pathlib import Path
+from os.path import join
+import itertools
+from statistics import mean
+import logging
+from datetime import datetime
+import json
+# %%
+# biopac_dir = '/Volumes/spacetop_projects_social/data/physio/01_raw-physio'#'/Volumes/spacetop/biopac/dartmouth/b04_finalbids/'
+# beh_dir =  '/Volumes/spacetop_projects_social/data/beh/d02_preproc-beh'# '/Volumes/spacetop_projects_social/data/d02_preproc-beh'
+# cuestudy_dir = '/Volumes/spacetop_projects_social' 
+# log_dir = join(cuestudy_dir, "scripts", "logcenter")
+
+discovery=1
+if discovery:
+    biopac_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/data/physio/01_raw-physio'#'/Volumes/spacetop/biopac/dartmouth/b04_finalbids/'
+    beh_dir =  '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/data/beh/d02_preproc-beh'# '/Volumes/spacetop_projects_social/data/d02_preproc-beh'
+    cuestudy_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social'
+    log_dir = join(cuestudy_dir, "scripts", "logcenter")
+else:
+    biopac_dir = '/Volumes/spacetop_projects_social/data/physio/01_raw-physio'#'/Volumes/spacetop/biopac/dartmouth/b04_finalbids/'
+    beh_dir =  '/Volumes/spacetop_projects_social/data/beh/d02_preproc-beh'# '/Volumes/spacetop_projects_social/data/d02_preproc-beh'
+    cuestudy_dir = '/Volumes/spacetop_projects_social' 
+    log_dir = join(cuestudy_dir, "scripts", "logcenter")
+# glob files
+save_dir = join(cuestudy_dir, 'data', 'physio', '02_preproc-physio')
+tonic_flist = glob.glob(join(cuestudy_dir, 'data', 'physio', '02_preproc-physio', '*', '*', 
+f"*_epochstart--1_epochend-8_physio-edatonic.csv"))
+tonic_group_df = pd.DataFrame()
+for tonic_fpath in tonic_flist:
+    tonic_df = pd.read_csv(tonic_fpath)
+    tonic_group_df = pd.concat([tonic_group_df, tonic_df])
+tonic_group_df.to_csv(join(save_dir, f"group_epochstart--1_epochend-8_physio-edatonic.csv"))
+with open(join(save_dir,'group_epochstart--1_epochend-8_physio-edatonic.json'), 'w', encoding='utf-8') as f:
+    json.dump(tonic_flist, f, ensure_ascii=False, indent=4)
+
+
+# %% Phasic: ________________________________________________________________________________
+phasic_flist = glob.glob(join(cuestudy_dir, 'data', 'physio', '02_preproc-physio', '*', '*', 
+f"*_epochstart-0_epochend-9_physio-phasictonic.csv"))
+phasic_group_df = pd.DataFrame()
+for phasic_fpath in phasic_flist:
+    phasic_df = pd.read_csv(phasic_fpath)
+    phasic_group_df = pd.concat([phasic_group_df, phasic_df])
+phasic_group_df.to_csv(join(save_dir, f"group_epochstart-0_epochend-9_physio-phasic.csv"))
+with open(join(save_dir,'group_epochstart-0_epochend-9_physio-phasic.json'), 'w', encoding='utf-8') as f:
+    json.dump(phasic_flist, f, ensure_ascii=False, indent=4)
+# stack files
+# savee filename in mettadata
