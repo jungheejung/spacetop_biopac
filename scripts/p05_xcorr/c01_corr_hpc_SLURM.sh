0435f6fe64043303902a0f35fc4c98902bf42d81
jungheejung <heejung.jung@colorado.edu>
2024-03-03 01:23:16 -0500

    slurm parameter

--- scripts/p05_xcorr/c01_corr_hpc_SLURM.sh
+++ scripts/p05_xcorr/c01_corr_hpc_SLURM.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 #SBATCH --job-name=glm
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
+#SBATCH --ntasks=12
 #SBATCH --mem-per-cpu=8gb
 #SBATCH --time=01:00:00
 #SBATCH -o ./log_xcorr/xcorr_%A_%a.o
 #SBATCH -e ./log_xcorr/xcorr_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-133%10
+#SBATCH --array=1-133%30
 
 conda activate biopac
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
@@ -16,7 +16,7 @@ ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
 PHYSIO="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/physio/physio03_bids/task-cue"
 FMRIPREP="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep/results/fmriprep/"
-SAVE="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/analysis/physio/xcorr"
+SAVE="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/analysis/physio/xcorr_noZ"
 
 python ${PWD}/c01_corr_hpc.py \
 --slurm-id ${ID} \
