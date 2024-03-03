7d6b6732fa3c07659fe2d979f5fb46fdd59c6ebd
jungheejung <heejung.jung@colorado.edu>
2024-03-03 18:21:08 -0500

    DEV: calculate xcorr PER TRIAL within roi

--- scripts/p05_xcorr/c01_corr_hpc_pertrial.py
+++ scripts/p05_xcorr/c01_corr_hpc_pertrial.py
@@ -208,7 +208,8 @@ for i, physio_fname in enumerate(physio_flist):
     # 4. loop through ROI and calculate xcorr ________________________________________________
     # 4-1. create dataframe to store ROI data
     print(f"step 4: calculate xcorr _____________")
-    roi_df = pd.DataFrame(index=range(time_series.shape[1]), columns=['sub', 'ses', 'run', 'roi', 'Maximum Correlation Value', 'Time Lag (s)'])
+    # roi_df = pd.DataFrame(index=range(time_series.shape[1]), columns=['sub', 'ses', 'run', 'roi', 'trial','Maximum Correlation Value', 'Time Lag (s)'])
+    roi_data = []
     for roi_ind in range(time_series.shape[1]):
         # remove outlier
         roi = time_series.T[roi_ind]
@@ -242,100 +243,144 @@ for i, physio_fname in enumerate(physio_flist):
             interp_func = interp1d(time_points[valid], data[valid], kind='linear', fill_value="extrapolate")
             return interp_func(time_points)
 
+
+# identify trials _____
+        df_metajson = '/Volumes/spacetop_projects_cue/analysis/physio/physio01_SCL_25s/sub-0081/ses-04/sub-0081_ses-04_run-03_runtype-pain_samplingrate-2000_onset.json'
+
+        with open(df_metajson, 'r') as file:
+            df_meta = json.load(file)
+            print(df_meta)
+
+        conversion_factor = source_samplingrate / fmri_samplingrate
+        converted_df_meta = {}
+        for event_type, times in df_meta.items():
+            converted_df_meta[event_type] = {}
+            for key, value in times.items():
+                converted_df_meta[event_type][key] = [x / conversion_factor for x in value]
+        # extract stimuli time from onset values
+        event_stimuli_start = converted_df_meta['event_stimuli']['start']
+        event_stimuli_stop =  converted_df_meta['event_stimuli']['stop']
+        padding_seconds = 5  # seconds
+        padding_samples = padding_seconds * fmri_samplingrate  # Convert seconds to samples based on sampling rate
+        beh_meta = pd.read_csv(join()'/Volumes/spacetop_projects_cue/analysis/physio/physio01_SCL_25s/sub-0081/ses-04/sub-0081_ses-04_run-03_runtype-pain_epochstart--3_epochend-20_physio-scl.csv')
+
+# extract physio and fmri data based on onset time
+        data1 = physio_shave.squeeze()
+        data2 = fmri_shave.squeeze()
+        physio_stim = []
+        fmri_stim = []
+
+
+        for i, (start, stop, cue_type) in enumerate(zip(event_stimuli_start, event_stimuli_stop)):
+            start_index = max(0, int(start) - padding_samples)
+            stop_index = min(len(data1) - 1, np.round(stop)  + padding_samples)
+            physio_segment = data1.loc[int(start_index):int(stop_index), 'x0']
+            fmri_segment = data2.loc[int(start_index):int(stop_index), 'x0']
+            physio_stim.append(physio_segment)
+            fmri_stim.append(fmri_segment)
+ 
         # Interpolate missing values
         # data1 = interpolate_data(physio_standardized)
         # data2 = interpolate_data(fmri_standardized)
+        for i in np.arange(len(physio_stim)):
 
-        data1 = physio_shave.squeeze()
-        data2 = fmri_shave.squeeze()
-        # 4-2. plot parameters
-        fig = plt.figure(figsize=(16, 8))
-        gs = gridspec.GridSpec(2, 4, figure=fig)
-        ax1 = fig.add_subplot(gs[0, :]) # Wide subplot on row 1
-        ax2 = fig.add_subplot(gs[1, 0]) # Subplot 1 on row 2
-        ax3 = fig.add_subplot(gs[1, 1]) # Subplot 2 on row 2
-        ax4 = fig.add_subplot(gs[1, 2]) # Subplot 3 on row 2
-        ax5 = fig.add_subplot(gs[1, 3]) # Subplot 3 on row 2
+            # 4-2. plot parameters
+            fig = plt.figure(figsize=(16, 8))
+            gs = gridspec.GridSpec(2, 4, figure=fig)
+            ax1 = fig.add_subplot(gs[0, :]) # Wide subplot on row 1
+            ax2 = fig.add_subplot(gs[1, 0]) # Subplot 1 on row 2
+            ax3 = fig.add_subplot(gs[1, 1]) # Subplot 2 on row 2
+            ax4 = fig.add_subplot(gs[1, 2]) # Subplot 3 on row 2
+            ax5 = fig.add_subplot(gs[1, 3]) # Subplot 3 on row 2
 
-        # 4-2.A: Plot raw signals ______________________________
-        ax1.plot(tvec, data1, 'r', linewidth=1, alpha=0.7)
-        ax1.plot(tvec, data2, 'b', linewidth=1, alpha=0.7)
-        ax1.legend(['physio', f'fmri ROI'])
-        ax1.set_xlabel('time (s)')
-        ax1.set_ylabel('amplitude normalized (a.u.)')
-        ax1.set_title('raw signals')
+            # 4-2.A: Plot raw signals ______________________________
+            ax1.plot(tvec, data1, 'r', linewidth=1, alpha=0.7)
+            ax1.plot(tvec, data2, 'b', linewidth=1, alpha=0.7)
+            ax1.legend(['physio', f'fmri ROI'])
+            ax1.set_xlabel('time (s)')
+            ax1.set_ylabel('amplitude normalized (a.u.)')
+            ax1.set_title('raw signals')
 
-        # 4-2.B:  Compute and plot PSD ______________________________
-        ws = int(Fs * 15)
-        window = hann(ws)
-        noverlap = ws // 2
-        nfft = len(tvec)
+            # 4-2.B:  Compute and plot PSD ______________________________
+            ws = int(Fs * 15)
+            window = hann(ws)
+            noverlap = ws // 2
+            nfft = len(tvec)
 
-        f, Pxx = welch(data1, Fs, window=window, noverlap=noverlap, nfft=nfft)
-        _, Pyy = welch(data2, Fs, window=window, noverlap=noverlap, nfft=nfft)
+            f, Pxx = welch(data1, Fs, window=window, noverlap=noverlap, nfft=nfft)
+            _, Pyy = welch(data2, Fs, window=window, noverlap=noverlap, nfft=nfft)
 
-        ax2.plot(f, np.abs(Pxx), 'r', f, np.abs(Pyy), 'b')
-        ax2.set_xlim([0, 1])
-        ax2.set_xlabel('Frequency (Hz)')
-        ax2.set_ylabel('power')
-        ax2.set_title('PSD')
+            ax2.plot(f, np.abs(Pxx), 'r', f, np.abs(Pyy), 'b')
+            ax2.set_xlim([0, 1])
+            ax2.set_xlabel('Frequency (Hz)')
+            ax2.set_ylabel('power')
+            ax2.set_title('PSD')
 
-        # 4-2.C: Compute and plot cross-spectrum
-        f, Pxy = csd(data1, data2, Fs, window=window, noverlap=noverlap, nfft=nfft)
-        ax3.plot(f, np.abs(Pxy))
-        ax3.set_xlim([0, Fs/2]) # Nyquist frequency is the upper bound
-        ax3.set_xlabel('Frequency (Hz)')
-        ax3.set_ylabel('power')
-        ax3.set_title('cross-spectrum')
+            # 4-2.C: Compute and plot cross-spectrum
+            f, Pxy = csd(data1, data2, Fs, window=window, noverlap=noverlap, nfft=nfft)
+            ax3.plot(f, np.abs(Pxy))
+            ax3.set_xlim([0, Fs/2]) # Nyquist frequency is the upper bound
+            ax3.set_xlabel('Frequency (Hz)')
+            ax3.set_ylabel('power')
+            ax3.set_title('cross-spectrum')
 
-        # 4-2.D: Compute and plot cross-correlation
-        maxlags = int(Fs * 30)
-        acf = correlate(data1, data2, mode='full', method='auto') # matlab: xcorr
-        # acf /= len(data1)  # Normalizing
-        norm_factor = np.sqrt(np.sum(data1**2) * np.sum(data2**2))
-        ccf = acf / norm_factor
-        lags = np.arange(-maxlags, maxlags + 1) * (1./Fs)
+            # 4-2.D: Compute and plot cross-correlation
+            maxlags = int(Fs * 30)
+            acf = correlate(data1, data2, mode='full', method='auto') # matlab: xcorr
+            # acf /= len(data1)  # Normalizing
+            norm_factor = np.sqrt(np.sum(data1**2) * np.sum(data2**2))
+            ccf = acf / norm_factor
+            lags = np.arange(-maxlags, maxlags + 1) * (1./Fs)
 
-        max_lags_index = np.where((lags >= -maxlags) & (lags <= maxlags))
-        ccf = ccf[max_lags_index]
-        lags = lags[max_lags_index]
-        convert_lags = lags*(1./Fs);
-        # ax4.plot(lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1], acf[len(acf)//2-maxlags:len(acf)//2+maxlags+1])
-        # ax4.plot(lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1], acf_normalized[len(acf_normalized)//2-maxlags:len(acf_normalized)//2+maxlags+1])
-        ax4.plot(convert_lags, ccf)
-        ax4.grid(True)
-        ax4.set_xlabel('time lag (s)')
-        ax4.set_ylabel('correlation (r)')
-        ax4.set_title('xcorr')
-        ax4.text(0.5, -0.2, '<-- SCR leads       BOLD leads -->', ha='center', va='center', transform=ax4.transAxes)
+            max_lags_index = np.where((lags >= -maxlags) & (lags <= maxlags))
+            ccf = ccf[max_lags_index]
+            lags = lags[max_lags_index]
+            convert_lags = lags*(1./Fs);
+            # ax4.plot(lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1], acf[len(acf)//2-maxlags:len(acf)//2+maxlags+1])
+            # ax4.plot(lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1], acf_normalized[len(acf_normalized)//2-maxlags:len(acf_normalized)//2+maxlags+1])
+            ax4.plot(convert_lags, ccf)
+            ax4.grid(True)
+            ax4.set_xlabel('time lag (s)')
+            ax4.set_ylabel('correlation (r)')
+            ax4.set_title('xcorr')
+            ax4.text(0.5, -0.2, '<-- SCR leads       BOLD leads -->', ha='center', va='center', transform=ax4.transAxes)
 
 
-        # 4-2.E: Coherence
-        f_coh, Cxy = coherence(data1, data2, Fs, window=hann(ws), noverlap=noverlap, nfft=nfft)
-        ax5.plot(f_coh, Cxy)
-        ax5.set_xlim([0, Fs/2])  # Limit to Nyquist frequency
-        ax5.set_xlabel('frequency [Hz]')
-        ax5.set_ylabel('Coherence')
-        ax5.set_title('Coherence Spectrum')
-        plt.tight_layout()
-        sns.despine()
-        plt.show()
-        # fig.savefig(join(save_dir, f"{sub}_{ses}_{run}_runtype-{runtype}_roi-{roi}_xcorr-fmri-physio.png"))
-        # plt.close(fig)
-        # calculate xcorr and save in dataframe _________________________
-        # Slicing acf and lags for the plot range
-        acf_sliced = ccf[len(ccf)//2-maxlags:len(ccf)//2+maxlags+1]
-        lags_sliced = lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1]
+            # 4-2.E: Coherence
+            f_coh, Cxy = coherence(data1, data2, Fs, window=hann(ws), noverlap=noverlap, nfft=nfft)
+            ax5.plot(f_coh, Cxy)
+            ax5.set_xlim([0, Fs/2])  # Limit to Nyquist frequency
+            ax5.set_xlabel('frequency [Hz]')
+            ax5.set_ylabel('Coherence')
+            ax5.set_title('Coherence Spectrum')
+            plt.tight_layout()
+            sns.despine()
+            plt.show()
+            # fig.savefig(join(save_dir, f"{sub}_{ses}_{run}_runtype-{runtype}_roi-{roi}_xcorr-fmri-physio.png"))
+            # plt.close(fig)
+            # calculate xcorr and save in dataframe _________________________
+            # Slicing acf and lags for the plot range
+            acf_sliced = ccf[len(ccf)//2-maxlags:len(ccf)//2+maxlags+1]
+            lags_sliced = lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1]
 
-        # Find the maximum correlation value and corresponding time lag
-        absolute_values = [abs(number) for number in acf_sliced]  # Convert all numbers to their 
-        max_acf_value = np.max(absolute_values)
-        max_acf_index = np.argmax(absolute_values)
-        max_lag_time = lags_sliced[max_acf_index]
+            # Find the maximum correlation value and corresponding time lag
+            absolute_values = [abs(number) for number in acf_sliced]  # Convert all numbers to their 
+            max_acf_value = np.max(absolute_values)
+            max_acf_index = np.argmax(absolute_values)
+            max_lag_time = lags_sliced[max_acf_index]
 
         # Create a DataFrame to store these values _________________________
-        roi_df.iloc[roi] = [sub, ses, run, roi, max_acf_value, max_lag_time]
-        save_fname = join(save_top_dir, f"{sub}_{ses}_{run}_runtype-{runtype}_xcorr-fmri-physio.tsv")
-        roi_df.to_csv(save_fname,sep='\t')
+            roi_data.append({
+                'sub': sub,
+                'ses': ses,
+                'run': run,
+                'roi': roi,
+                'index': i,  # Assuming 'i' is an index, renamed to avoid confusion
+                'max_acf_value': max_acf_value,
+                'max_lag_time': max_lag_time
+            })
+    roi_df = pd.DataFrame(roi_data)
+    save_fname = join(save_top_dir, f"{sub}_{ses}_{run}_runtype-{runtype}_xcorr-fmri-physio.tsv")
+    roi_df.to_csv(save_fname,sep='\t')
 plt.close('all')
 print("complete!")
