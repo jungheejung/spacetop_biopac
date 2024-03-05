2297c5f6cc01b1e7ea50cdf54036a8e433efea3a
jungheejung <heejung.jung@colorado.edu>
2024-03-05 11:36:36 -0500

    DEV: new bandpass filter and use filtered data

--- scripts/p05_xcorr/c01_corr_hpc.py
+++ scripts/p05_xcorr/c01_corr_hpc.py
@@ -85,18 +85,14 @@ def interpolate_data(data):
     interp_func = interp1d(time_points[valid], data[valid], kind='linear', fill_value="extrapolate")
     return interp_func(time_points)
 
-# %% 1. glob physio data
 
-# * baseline corrected
-# * detrend
-# * low pass filter of 1
-
-physio_flist = glob.glob(join(physio_dir, '**', '{sub}_ses-*_run-*_runtype-{runtype}_epochstart--3_epochend-20_baselinecorrect-True_samplingrate-25_physio-eda.tsv'))
+# %% 1. glob physio data _______________________________________________________
+physio_flist = glob.glob(join(physio_dir, '**', '{sub}_ses-*_run-*_runtype-{runtype}_epochstart--3_epochend-20_baselinecorrect-False_samplingrate-25_physio-eda.tsv'))
 
 # %%
 for i, physio_fname in enumerate(physio_flist):
 
-    # 2. Extract bids info
+    # 2. Extract bids info _____________________________________________________
     matches = re.search(r"sub-(\d+)/ses-(\d+)/.*_run-(\d+)-", physio_fname)
     if matches:
         sub = f"sub-{matches.group(1)}"
@@ -105,18 +101,21 @@ for i, physio_fname in enumerate(physio_flist):
     else:
         sub, ses, run = None, None, None
     print(f"step 2: extract bids info {sub} {ses} {run}")
-    # 3-1. load physio data ________________________________________________
-    df = pd.read_csv(physio_fname, sep='\t')
-    # resample physio data (2000hz to 25hz)
-    source_samplingrate=2000
-    dest_samplingrate=25
-    resamp = nk.signal_resample(
-                df['physio_eda'].to_numpy(),  method='interpolation', sampling_rate=source_samplingrate, desired_sampling_rate=dest_samplingrate)
 
-    # filter
-    # butterworth
-    # detrend
-    # 3-2. load brain data ________________________________________________
+
+    # 3-1. load physio data ____________________________________________________
+    df = pd.read_csv(physio_fname, sep='\t')
+
+    TR=0.46
+    source_samplingrate = 25
+    dest_samplingrate = 1/TR
+    physio_tr = nk.signal_resample(
+                df['physio_eda'].to_numpy(),  method='interpolation', 
+                sampling_rate=source_samplingrate, 
+                desired_sampling_rate=dest_samplingrate)
+
+
+    # 3-2. load brain data _____________________________________________________
     print(f"step 3-2: load brain data _____________")
     fmri_fname = join(fmriprep_dir,sub, ses, 'func', f"{sub}_{ses}_task-social_acq-mb8_run-{int(matches.group(3))}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz" )
 
@@ -124,15 +123,14 @@ for i, physio_fname in enumerate(physio_flist):
     confounds_fname = join(fmriprep_dir, sub, ses, 'func', f'{sub}_{ses}_task-social_acq-mb8_run-{int(matches.group(3))}_desc-confounds_timeseries.tsv')
 
     schaefer = datasets.fetch_atlas_schaefer_2018(n_rois=400, resolution_mm=2, data_dir=save_top_dir)
-    #masker = NiftiLabelsMasker(labels_img=schaefer['maps'], 
-    #                        labels=schaefer['labels']
-                            #    standardize=True, 
-                            #    high_pass=128,
-                            #    t_r=0.46
-                            #)
-    masker = NiftiLabelsMasker(labels_img=join(save_top_dir, 'schaefer_2018', 'Schaefer2018_400Parcels_7Networks_order_FSLMNI152_2mm.nii.gz'))
-    #        labels=join(save_top_dir, 'schaefer_2018', 'Schaefer2018_400Parcels_7Networks_order.txt'))
-    # 3-3. subset confounds
+
+    masker = NiftiLabelsMasker(labels_img=join(save_top_dir, 'schaefer_2018', 'Schaefer2018_400Parcels_7Networks_order_FSLMNI152_2mm.nii.gz'),
+                               standardize=True,
+                               high_pass=1/128,
+                               t_r=0.46)
+    
+
+    # 3-3. subset confounds ____________________________________________________
     print(f"3-3 confound subset from fmriprep")
     confounds = pd.read_csv(confounds_fname,sep='\t')
     filter_col = [col for col in confounds if col.startswith("motion")]
@@ -182,63 +180,50 @@ for i, physio_fname in enumerate(physio_flist):
     print(subset_confounds.head())
     time_series = masker.fit_transform(fmri_fname, confounds=subset_confounds.fillna(subset_confounds.median()))
 
-    # 3-4. resample physio to fmri TR
-    TR=0.46
-    fmri_samplingrate = 1/0.46
-    # resamp physio data to TR sampling rate
-    physio_tr = nk.signal_resample(
-                df['physio_eda'].to_numpy(),  method='interpolation', sampling_rate=source_samplingrate, desired_sampling_rate=fmri_samplingrate)
-    physio_center = physio_tr - np.nanmean(physio_tr)
-    physio_filter = nk.signal_filter(physio_center, 
-                                     sampling_rate=dest_samplingrate,
-                                     highcut=1,
-                                     method="butterworth",
-                                     order=2)
-    physio_detrend = nk.signal_detrend(physio_filter, 
-                                       method="polynomial", 
-                                       order=0)
+    # 3-4. resample physio to fmri TR __________________________________________
 
-    # 4. loop through ROI and calculate xcorr ________________________________________________
+    # resamp physio data to TR sampling rate
+    # physio_tr = nk.signal_resample(
+    #             df['physio_eda'].to_numpy(),  method='interpolation', sampling_rate=source_samplingrate, desired_sampling_rate=fmri_samplingrate)
+    # physio_center = physio_tr - np.nanmean(physio_tr)
+    # physio_filter = nk.signal_filter(physio_center, 
+    #                                  sampling_rate=dest_samplingrate,
+    #                                  highcut=1,
+    #                                  method="butterworth",
+    #                                  order=2)
+    # physio_detrend = nk.signal_detrend(physio_filter, 
+    #                                    method="polynomial", 
+    #                                    order=0)
+
+    # 4. loop through ROI and calculate xcorr __________________________________
     # 4-1. create dataframe to store ROI data
     print(f"step 4: calculate xcorr _____________")
     roi_df = pd.DataFrame(index=range(time_series.shape[1]), columns=['sub', 'ses', 'run', 'roi', 'Maximum Correlation Value', 'Time Lag (s)'])
     for roi in range(time_series.shape[1]):
-        # remove outlier
+        
         roi = time_series.T[roi]
-
-
+        # remove outlier _______________________________________________________
         fmri_outlier = winsorize_mad(roi, threshold=7)
-        physio_outlier = winsorize_mad(physio_detrend, threshold=7)
-        # fmri_outlier = interpolate_data(winsor_physio)
+        physio_outlier = winsorize_mad(physio_tr, threshold=7)
 
-        # 4-2. plot and save
 
+        # 4-2. plot and save ___________________________________________________
         Fs = 1/TR #1/TR
         
-        physio_standardized = physio_outlier - np.nanmean(physio_outlier)  / np.nanstd(physio_outlier)
+        physio_standardized = physio_outlier - np.nanmean(physio_outlier)/np.nanstd(physio_outlier)
         fmri_standardized = fmri_outlier - np.nanmean(fmri_outlier)/np.nanstd(fmri_outlier)
         total_length = len(fmri_standardized)
         fmri_standardized = fmri_standardized[6:]
         physio_standardized = physio_standardized[6:total_length] 
         tvec = np.arange(0, len(physio_standardized) / Fs, 1/Fs)
         print(f"tvec: {len(tvec)}, physio:{physio_standardized.shape}, fmri:{fmri_standardized.shape}")
-        from scipy.interpolate import interp1d
-        # mean_data1 = np.nanmean(physio_standardized)
-        # mean_data2 = np.nanmean(fmri_standardized)
-
-        # # Replace NaN values with the computed mean
-        # data1 = np.where(np.isnan(physio_standardized), mean_data1, physio_standardized)
-        # data2 = np.where(np.isnan(fmri_standardized), mean_data2, fmri_standardized)
-        def interpolate_data(data):
-            time_points = np.arange(len(data))
-            valid = ~np.isnan(data)  # Mask of valid (non-NaN) data points
-            interp_func = interp1d(time_points[valid], data[valid], kind='linear', fill_value="extrapolate")
-            return interp_func(time_points)
 
         # Interpolate missing values
-        data1 = interpolate_data(physio_standardized)
-        data2 = interpolate_data(fmri_standardized)
-        # 4-2. plot parameters
+        data1 = physio_standardized #interpolate_data(physio_standardized)
+        data2 = fmri_standardized# interpolate_data(fmri_standardized)
+
+
+        # 4-2. plot parameters _________________________________________________
         fig = plt.figure(figsize=(16, 8))
         gs = gridspec.GridSpec(2, 4, figure=fig)
         ax1 = fig.add_subplot(gs[0, :]) # Wide subplot on row 1
@@ -247,7 +232,8 @@ for i, physio_fname in enumerate(physio_flist):
         ax4 = fig.add_subplot(gs[1, 2]) # Subplot 3 on row 2
         ax5 = fig.add_subplot(gs[1, 3]) # Subplot 3 on row 2
 
-        # 4-2.A: Plot raw signals ______________________________
+
+        # 4-2.A: Plot raw signals ______________________________________________
         ax1.plot(tvec, data1, 'r', linewidth=1, alpha=0.7)
         ax1.plot(tvec, data2, 'b', linewidth=1, alpha=0.7)
         ax1.legend(['physio', f'fmri ROI'])
@@ -255,7 +241,8 @@ for i, physio_fname in enumerate(physio_flist):
         ax1.set_ylabel('amplitude normalized (a.u.)')
         ax1.set_title('raw signals')
 
-        # 4-2.B:  Compute and plot PSD ______________________________
+
+        # 4-2.B:  Compute and plot PSD _________________________________________
         ws = int(Fs * 15)
         window = hann(ws)
         noverlap = ws // 2
@@ -270,7 +257,8 @@ for i, physio_fname in enumerate(physio_flist):
         ax2.set_ylabel('power')
         ax2.set_title('PSD')
 
-        # 4-2.C: Compute and plot cross-spectrum
+
+        # 4-2.C: Compute and plot cross-spectrum _______________________________
         f, Pxy = csd(data1, data2, Fs, window=window, noverlap=noverlap, nfft=nfft)
         ax3.plot(f, np.abs(Pxy))
         ax3.set_xlim([0, Fs/2]) # Nyquist frequency is the upper bound
@@ -278,10 +266,10 @@ for i, physio_fname in enumerate(physio_flist):
         ax3.set_ylabel('power')
         ax3.set_title('cross-spectrum')
 
-        # 4-2.D: Compute and plot cross-correlation
-        maxlags = int(Fs * 30)
+
+        # 4-2.D: Compute and plot cross-correlation ____________________________
+        maxlags = int(Fs * 15)
         acf = correlate(data1, data2, mode='full', method='auto') # matlab: xcorr
-        # acf /= len(data1)  # Normalizing
         norm_factor = np.sqrt(np.sum(data1**2) * np.sum(data2**2))
         ccf = acf / norm_factor
         lags = np.arange(-maxlags, maxlags + 1) * (1./Fs)
@@ -289,9 +277,8 @@ for i, physio_fname in enumerate(physio_flist):
         max_lags_index = np.where((lags >= -maxlags) & (lags <= maxlags))
         ccf = ccf[max_lags_index]
         lags = lags[max_lags_index]
-        convert_lags = lags*(1./Fs);
-        # ax4.plot(lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1], acf[len(acf)//2-maxlags:len(acf)//2+maxlags+1])
-        # ax4.plot(lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1], acf_normalized[len(acf_normalized)//2-maxlags:len(acf_normalized)//2+maxlags+1])
+        convert_lags = lags*(1./Fs)
+
         ax4.plot(convert_lags, ccf)
         ax4.grid(True)
         ax4.set_xlabel('time lag (s)')
@@ -300,7 +287,7 @@ for i, physio_fname in enumerate(physio_flist):
         ax4.text(0.5, -0.2, '<-- SCR leads       BOLD leads -->', ha='center', va='center', transform=ax4.transAxes)
 
 
-        # 4-2.E: Coherence
+        # 4-2.E: Coherence _____________________________________________________
         f_coh, Cxy = coherence(data1, data2, Fs, window=hann(ws), noverlap=noverlap, nfft=nfft)
         ax5.plot(f_coh, Cxy)
         ax5.set_xlim([0, Fs/2])  # Limit to Nyquist frequency
@@ -312,7 +299,9 @@ for i, physio_fname in enumerate(physio_flist):
         # plt.show()
         fig.savefig(join(save_dir, f"{sub}_{ses}_{run}_runtype-{runtype}_roi-{roi}_xcorr-fmri-physio.png"))
         plt.close(fig)
-        # calculate xcorr and save in dataframe _________________________
+
+
+        # calculate xcorr and save in dataframe ________________________________
         # Slicing acf and lags for the plot range
         acf_sliced = ccf[len(ccf)//2-maxlags:len(ccf)//2+maxlags+1]
         lags_sliced = lags[len(lags)//2-maxlags:len(lags)//2+maxlags+1]
@@ -323,9 +312,10 @@ for i, physio_fname in enumerate(physio_flist):
         max_acf_index = np.argmax(absolute_values)
         max_lag_time = lags_sliced[max_acf_index]
 
-        # Create a DataFrame to store these values _________________________
+        # Create a DataFrame to store these values _____________________________
         roi_df.iloc[roi] = [sub, ses, run, roi, max_acf_value, max_lag_time]
         save_fname = join(save_top_dir, f"{sub}_{ses}_{run}_runtype-{runtype}_xcorr-fmri-physio.tsv")
         roi_df.to_csv(save_fname,sep='\t')
+
 plt.close('all')
 print("complete!")
